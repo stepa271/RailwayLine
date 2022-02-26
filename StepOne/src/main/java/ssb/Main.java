51f0142f679e60b847e8d588500f8fafbbb4bb19
Vladimir S <9949498@gmail.com>
2022-02-26 16:28:51 +0300

    Драйв менеджер прописан, в помнике добавлена зависимость.СОединение через тестовый класс установлено с БД.

---
+++ StepOne/src/main/java/ssb/Main.java
@@ -0,0 +1,21 @@
+package ssb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://hs-02:3306/?serverTimezone=Europe/Moscow&useSSL=false";
+        String username = "rootroot";
+        String password = "rootroot";
+        System.out.println("Connecting...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
