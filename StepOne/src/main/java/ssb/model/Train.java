b86e451b8591cc4a89bf34eca97308d85e11fb78
Vladimir S <9949498@gmail.com>
2022-02-26 17:04:02 +0300

    db.properties в ресурсы

--- StepOne/src/main/java/ssb/model/Train.java
+++ StepOne/src/main/java/ssb/model/Train.java
@@ -1,12 +1,28 @@
 package ssb.model;
 
 
+import javax.persistence.*;
+
 /* POJO (Plain Old Java Object) */
+@Entity
+@Table(name="train")
 public class Train {
+    @Id
+    @Column(name = "idtrain")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name ="number_train")
     private String numberTrain;
+
+    @Column(name = "id_station")
     private int idStation;
+
+    @Column(name = "count_place")
     private int countPlace;
+
+
+
 
     public String getNumberTrain() {
         return numberTrain;
